e073e05c8a5831764bab6e7168a81c0b0aa6fcc0
Michele Micunco <michele.micunco@accenture.com>
2018-09-06 15:07:49 +0200

    fix grid

--- public/app.js
+++ public/app.js
@@ -3316,211 +3316,251 @@
                     });
                 });
             };
-            
+
             /***************************************************************************************************
              *                      TAB FANTACALCIO
              **************************************************************************************************/
-            
-            $scope.fantacalcio = [{id: 1, name: "FANTAFIGHETTINO"},{id: 2, name: "FANTAMARELLI" },{id: 3, name: "FANTABOMBOLACCI"}];
-            
+
+            $scope.fantacalcio = [{
+                id: 1,
+                name: "FANTAFIGHETTINO"
+            }, {
+                id: 2,
+                name: "FANTAMARELLI"
+            }, {
+                id: 3,
+                name: "FANTABOMBOLACCI"
+            }];
+
             $scope.gridOptionsPandathinaikos = {
-                    columnVirtualizationThreshold: 100,                
-                minRowsToShow: 25,                
-                enableFiltering: true,                                
-                selectionRowHeaderWidth: 35,                
+                columnVirtualizationThreshold: 100,
+                minRowsToShow: 25,
+                enableFiltering: true,
+                selectionRowHeaderWidth: 35,
                 enableColumnMenus: false,
                 columnDefs: [{
-                        name: 'ID',
-                        displayName: 'ID',
-                        field: 'ID',
-                        width: 50,
-                        pinnedLeft: true
-                },{
-                        name: 'RUOLO',
-                        displayName: 'R',
-                        field: 'RUOLO',
-                        width: 30,
-                        pinnedLeft: true
-                },{
-                        name: 'GIOCATORE',
-                        displayName: 'Giocatore',
-                        field: 'GIOCATORE',
-                        width: '*',
-                        pinnedLeft: true
-                },{
-                        name: 'TEAM_NAME',
-                        displayName: 'Squadra',
-                        field: 'TEAM_NAME',
-                        width: 140,
-                        pinnedLeft: true
-                },{
-                        name: 'VALORE_ACQUISTO',
-                        displayName: 'Val. Acqs.',
-                        field: 'VALORE_ACQUISTO',
-                        width: 50
-                },{
-                        name: 'PG_TOT',
-                        displayName: 'PT',
-                        field: 'PG_TOT',
-                        width: 50
-                },{
-                        name: 'MEDIA_MAGIC_TOT',
-                        displayName: 'Magic Media',
-                        field: 'MEDIA_MAGIC_TOT',
-                        width: 50
-                },{
-                        name: 'MEDIA_VOTO_TOT',
-                        displayName: 'Media Voto',
-                        field: 'MEDIA_VOTO_TOT',
-                        width: 50
-                },{
-                        name: 'GOL_TOT',
-                        displayName: 'Gol',
-                        field: 'GOL_TOT',
-                        width: 50
-                },{
-                        name: 'ASSIST_TOT',
-                        displayName: 'Assist',
-                        field: 'ASSIST_TOT',
-                        width: 50
-                },{
-                        name: 'RIGORE_TOT',
-                        displayName: 'Rig.',
-                        field: 'RIGORE_TOT',
-                        width: 50
-                },{
-                        name: 'AUTORETE_TOT',
-                        displayName: 'Autorete',
-                        field: 'AUTORETE_TOT',
-                        width: 50
-                },{
-                        name: 'AMMONIZIONE_TOT',
-                        displayName: 'Amm.',
-                        field: 'AMMONIZIONE_TOT',
-                        width: 50
-                },{
-                        name: 'ESPULSIONE_TOT',
-                        displayName: 'Esp.',
-                        field: 'ESPULSIONE_TOT',
-                        width: 50
-                },{
-                        name: 'PG_HOME',
-                        displayName: 'PTH',
-                        field: 'PG_HOME',
-                        width: 50
-                },{
-                        name: 'MEDIA_MAGIC_PUNTI_HOME',
-                        displayName: 'Magic Media Home',
-                        field: 'MEDIA_MAGIC_PUNTI_HOME',
-                        width: 50
-                },{
-                        name: 'MEDIA_VOTO_PAGELLA_HOME',
-                        displayName: 'Media Voto Home',
-                        field: 'MEDIA_VOTO_PAGELLA_HOME',
-                        width: 50
-                },{
-                        name: 'GOL_HOME',
-                        displayName: 'Gol Home',
-                        field: 'GOL_HOME',
-                        width: 50
-                },{
-                        name: 'ASSIST_HOME',
-                        displayName: 'Assist Home',
-                        field: 'ASSIST_HOME',
-                        width: 50
-                },{
-                        name: 'RIGORE_HOME',
-                        displayName: 'Rig. Home',
-                        field: 'RIGORE_HOME',
-                        width: 50
-                },{
-                        name: 'AUTORETE_HOME',
-                        displayName: 'Autorete Home',
-                        field: 'AUTORETE_HOME',
-                        width: 50
-                },{
-                        name: 'AMMONIZIONE_HOME',
-                        displayName: 'Amm. Home',
-                        field: 'AMMONIZIONE_HOME',
-                        width: 50
-                },{
-                        name: 'ESPULSIONE_HOME',
-                        displayName: 'Esp. Home',
-                        field: 'ESPULSIONE_HOME',
-                        width: 50
-                },{
-                        name: 'PG_AWAY',
-                        displayName: 'PTH',
-                        field: 'PG_AWAY',
-                        width: 50
-                },{
-                        name: 'MEDIA_MAGIC_PUNTI_HOME',
-                        displayName: 'Magic Media AWAY',
-                        field: 'MEDIA_MAGIC_PUNTI_HOME',
-                        width: 50
-                },{
-                        name: 'MEDIA_VOTO_PAGELLA_HOME',
-                        displayName: 'Media Voto AWAY',
-                        field: 'MEDIA_VOTO_PAGELLA_HOME',
-                        width: 50
-                },{
-                        name: 'GOL_AWAY',
-                        displayName: 'Gol AWAY',
-                        field: 'GOL_AWAY',
-                        width: 50
-                },{
-                        name: 'ASSIST_AWAY',
-                        displayName: 'Assist AWAY',
-                        field: 'ASSIST_AWAY',
-                        width: 50
-                },{
-                        name: 'RIGORE_AWAY',
-                        displayName: 'Rig. AWAY',
-                        field: 'RIGORE_AWAY',
-                        width: 50
-                },{
-                        name: 'AUTORETE_AWAY',
-                        displayName: 'Autorete AWAY',
-                        field: 'AUTORETE_AWAY',
-                        width: 50
-                },{
-                        name: 'AMMONIZIONE_AWAY',
-                        displayName: 'Amm. AWAY',
-                        field: 'AMMONIZIONE_AWAY',
-                        width: 50
-                },{
-                        name: 'ESPULSIONE_AWAY',
-                        displayName: 'Esp. AWAY',
-                        field: 'ESPULSIONE_AWAY',
-                        width: 50
-                }]
-                };
-            
-            $scope.loadFantaRosa = function (){
-                                            
-               switch ($scope.fanta.value.id) {
-                    case 1:
-                       return $http.get('http://2.225.127.144:3001/fantafighettino').then(function (resp) {
-                           $scope.gridOptionsPandathinaikos.data = resp.data;
-                       });
-                       break;
-                    case 2:
-                       return $http.get('http://2.225.127.144:3001/fantamarelli').then(function (resp) {
-                           $scope.gridOptionsPandathinaikos.data = resp.data;
-                       });
-                       break;
-                    case 3:
-                       return $http.get('http://2.225.127.144:3001/fantabombolacci').then(function (resp) {
-                           $scope.gridOptionsPandathinaikos.data = resp.data;
-                       });
-                       break;
-                   default:
-                       return $http.get('http://2.225.127.144:3001/fantafighettino').then(function (resp) {
-                           $scope.gridOptionsPandathinaikos.data = resp.data;
-                       });                       
-               }                                                                    
+                    name: 'ID',
+                    displayName: 'ID',
+                    field: 'ID',
+                    width: 50,
+                    pinnedLeft: true
+                }, {
+                    name: 'RUOLO',
+                    displayName: 'R',
+                    field: 'RUOLO',
+                    width: 30,
+                    pinnedLeft: true
+                }, {
+                    name: 'GIOCATORE',
+                    displayName: 'Giocatore',
+                    field: 'GIOCATORE',
+                    width: '*',
+                    pinnedLeft: true
+                }, {
+                    name: 'TEAM_NAME',
+                    displayName: 'Squadra',
+                    field: 'TEAM_NAME',
+                    width: 140,
+                    pinnedLeft: true
+                }, {
+                    name: 'VALORE_ACQUISTO',
+                    displayName: 'Val. Acqs.',
+                    field: 'VALORE_ACQUISTO',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'PG_TOT',
+                    displayName: 'Gioc.',
+                    field: 'PG_TOT',
+                    width: 50
+                }, {
+                    name: 'MEDIA_MAGIC_TOT',
+                    displayName: 'Magic Media',
+                    field: 'MEDIA_MAGIC_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'MEDIA_VOTO_TOT',
+                    displayName: 'Media Voto',
+                    field: 'MEDIA_VOTO_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'GOL_TOT',
+                    displayName: 'Gol',
+                    field: 'GOL_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'ASSIST_TOT',
+                    displayName: 'Assist',
+                    field: 'ASSIST_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'RIGORE_TOT',
+                    displayName: 'Rig.',
+                    field: 'RIGORE_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'AUTORETE_TOT',
+                    displayName: 'Autorete',
+                    field: 'AUTORETE_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'AMMONIZIONE_TOT',
+                    displayName: 'Amm.',
+                    field: 'AMMONIZIONE_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'ESPULSIONE_TOT',
+                    displayName: 'Esp.',
+                    field: 'ESPULSIONE_TOT',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'PG_HOME',
+                    displayName: 'Gioc. Home',
+                    field: 'PG_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'MEDIA_MAGIC_PUNTI_HOME',
+                    displayName: 'Magic Media Home',
+                    field: 'MEDIA_MAGIC_PUNTI_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'MEDIA_VOTO_PAGELLA_HOME',
+                    displayName: 'Media Voto Home',
+                    field: 'MEDIA_VOTO_PAGELLA_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'GOL_HOME',
+                    displayName: 'Gol Home',
+                    field: 'GOL_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'ASSIST_HOME',
+                    displayName: 'Assist Home',
+                    field: 'ASSIST_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'RIGORE_HOME',
+                    displayName: 'Rig. Home',
+                    field: 'RIGORE_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'AUTORETE_HOME',
+                    displayName: 'Autorete Home',
+                    field: 'AUTORETE_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'AMMONIZIONE_HOME',
+                    displayName: 'Amm. Home',
+                    field: 'AMMONIZIONE_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'ESPULSIONE_HOME',
+                    displayName: 'Esp. Home',
+                    field: 'ESPULSIONE_HOME',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'PG_AWAY',
+                    displayName: 'Gioc. Away',
+                    field: 'PG_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'MEDIA_MAGIC_PUNTI_AWAY',
+                    displayName: 'Magic Media Away',
+                    field: 'MEDIA_MAGIC_PUNTI_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'MEDIA_VOTO_PAGELLA_AWAY',
+                    displayName: 'Media Voto Away',
+                    field: 'MEDIA_VOTO_PAGELLA_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'GOL_AWAY',
+                    displayName: 'Gol Away',
+                    field: 'GOL_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'ASSIST_AWAY',
+                    displayName: 'Assist Away',
+                    field: 'ASSIST_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'RIGORE_AWAY',
+                    displayName: 'Rig. Away',
+                    field: 'RIGORE_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'AUTORETE_AWAY',
+                    displayName: 'Autorete Away',
+                    field: 'AUTORETE_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'AMMONIZIONE_AWAY',
+                    displayName: 'Amm. Away',
+                    field: 'AMMONIZIONE_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }, {
+                    name: 'ESPULSIONE_AWAY',
+                    displayName: 'Esp. Away',
+                    field: 'ESPULSIONE_AWAY',
+                    cellClass: 'text-right',
+                    width: 50
+                }],
+                onRegisterApi: function (gridApi) {
+                    $scope.gridOptionsPandathinaikos.gridApi = gridApi;
+                }
             };
-            
+
+            $scope.loadFantaRosa = function () {
+
+                switch ($scope.fanta.value.id) {
+                    case 1:
+                        return $http.get('http://2.225.127.144:3001/fantafighettino').then(function (resp) {
+                            $scope.gridOptionsPandathinaikos.data = resp.data;
+                        });
+                        break;
+                    case 2:
+                        return $http.get('http://2.225.127.144:3001/fantamarelli').then(function (resp) {
+                            $scope.gridOptionsPandathinaikos.data = resp.data;
+                        });
+                        break;
+                    case 3:
+                        return $http.get('http://2.225.127.144:3001/fantabombolacci').then(function (resp) {
+                            $scope.gridOptionsPandathinaikos.data = resp.data;
+                        });
+                        break;
+                    default:
+                        return $http.get('http://2.225.127.144:3001/fantafighettino').then(function (resp) {
+                            $scope.gridOptionsPandathinaikos.data = resp.data;
+                        });
+                }
+                $interval($scope.gridOptionsPandathinaikos.gridApi.core.handleWindowResize, 100, 10);
+            };
+
 
           }])
         .filter('map', function () {
