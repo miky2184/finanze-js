0bedad0ec7540e3d7c5d44bd0e2db839bd0e5558
Michele Micunco <michele.micunco@accenture.com>
2025-07-08 09:12:08 +0200

    a

--- public/service/graficoService.js
+++ public/service/graficoService.js
@@ -42,8 +42,8 @@
                         key: value.label,
                         values: data.map(function (d) {
                             return {
-                                'label': d.anno,
-                                'value': d[key]
+                                'x': d.anno,
+                                'y': d[key]
                             };
                         }),
                         color: value.hex_color             
@@ -132,11 +132,14 @@
                 chart: {
                     type: 'lineChart',
                     height: 650,
-                    showControls: false,
-                    showValues: true,
-                    duration: 500,                       
-                    x: function(d){return d.label;},
-                    y: function(d){return d.value;},
+                    margin: {
+                        top: 20,
+                        right: 20,
+                        bottom: 60,
+                        left: 65
+                    },                    
+                    x: function(d){return d.x;},
+                    y: function(d){return d.y;},
                     xAxis: {
                         axisLabel: 'Year',
                         showMaxMin: false
@@ -146,15 +149,7 @@
                         tickFormat: function(d){
                             return d3.format(',.2f')(d);
                         }
-                    },
-                    legend: {
-                        margin: {
-                            top: 5,
-                            right: 35,
-                            bottom: 5,
-                            left: 0
-                        }
-                    },
+                    },                    
                     callback: function (chart) {
                         $timeout(function () {
                             d3.selectAll('.nvtooltip').style('opacity', 0);
